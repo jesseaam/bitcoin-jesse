cb1bd3fbf77fc78db80d495ae8edf5401278541e
jesse-marks <18119820+jesse-marks@users.noreply.github.com>
2022-07-07 20:56:58 -0400

    add templates with bootstrap

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 from application import app
-from flask import Flask
+from flask import Flask, render_template
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template("index.html")
