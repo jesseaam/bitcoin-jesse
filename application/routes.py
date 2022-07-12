bd513fbdc428afb535c6906f605d069d94309d27
jesse-marks <18119820+jesse-marks@users.noreply.github.com>
2022-07-12 18:09:27 -0400

    Add bitcoin price and block height.

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,18 @@
 from application import app
 from flask import Flask, render_template
+import requests
+import json
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    height = requests.get("https://blockstream.info/api/blocks/tip/height")
+    height = height.text
+
+    current_price = requests.get("https://www.bitstamp.net/api/v2/ticker/btcusd/")
+    current_price = json.loads(current_price.text)["last"]
+
+    # how many sats can you buy with a dollar?
+    sats = round(100_000_000 / float(current_price))
+
+    return render_template("index.html", height=height, price=current_price, sats=sats)
