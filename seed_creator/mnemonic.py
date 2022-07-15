684ca6482bb5f780c413f59f032ca05bb11a5da3
Jesse Marks <jmarks@RTI-108335.rti.ns>
2022-07-15 13:42:43 -0400

    get ndx of specific word in binary format

--- seed_creator/mnemonic.py
+++ seed_creator/mnemonic.py
@@ -17,13 +17,20 @@ from typing import List #, AnyStr, TypeVar, Union
 # generate the 1st 10 priv,pub, and pub-addresses
 # search for those address on the block-chain
 
+
+# note that this path will need to be modified slightly once it is called from within Flask framework.
 d = os.path.join(os.path.dirname(__file__), "wordlist_english.txt")
 with open(d, "r", encoding="utf-8") as f:
     wordlist = [w.strip() for w in f.readlines()]
 
-print(type(wordlist))
+#print(wordlist[0:5]) # ['abandon', 'ability', 'able', 'about', 'above']
 
-#print(base58.b58encode(b'hello world'))
+ndx = wordlist.index("ability")
+ndx = bin(ndx)[2:].zfill(11)
+print(ndx)
+
+
+print(base58.b58encode(b'hello world'))
 #print(base58.b58decode(b'StV1DL6CwTryKyV'))
 #print(base58.b58encode_check(b'hello world'))
 
