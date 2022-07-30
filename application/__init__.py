562534f474fdf70c821329a9f8bbf736ee355f8e
jesse-marks <18119820+jesse-marks@users.noreply.github.com>
2022-07-30 04:20:14 -0400

    set track modifications to false

--- application/__init__.py
+++ application/__init__.py
@@ -8,6 +8,7 @@ app.secret_key = b"0MjasdL5%37C-111@#" # https://flask.palletsprojects.com/en/1.
 # set up database config
 # default postgresql://YOURUSERNAME@localhost/YOURUSERNAME
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://jmarks@localhost/jmarks"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # adds significant overhead
 
 # create database object
 db = SQLAlchemy(app)
